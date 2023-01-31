8c1a671ce183ef25122eadbc33eef573a81300e8
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-01-31 13:22:49 +0700

    header & snap scroll

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+header_card_change = function() {
+    let xs = document.getElementsByClassName('card')
+    let i = 0, j = 0;
+    for (let x of xs) {
+        if (x.classList.contains('hide')) {
+            x.classList.toggle('hide')
+        }
+        if (x.classList.contains('show')) {
+            x.classList.toggle('show')
+            x.classList.toggle('hide')
+            j = i;
+            continue;
+        }
+        i++;
+    }
+    xs[(j+1)%xs.length].classList.toggle('show')
+}
+
+onload = function() { 
+    var header_card_change_handler = setInterval(header_card_change, 5000);
+}
+
+window.addEventListener('onload', onload);
